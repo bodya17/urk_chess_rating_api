d3cd685ed2d2b7577d8489df5b7ee6fe41145252
bodya17 <levantovychbogdan@gmail.com>
2017-07-02 22:30:14 +0300

    add some interesting queries

--- app.js
+++ app.js
@@ -16,15 +16,73 @@ app.get('/fed', function(req, res) {
     });
 });
 
-app.get('/national-masters', function(req, res) {
+app.get('/ratings/:title', function(req, res) {
     let query = Player.aggregate([
-        { $match: {title: /кмс/} },
+        { $match: {title: new RegExp(req.params.title)} },
         { $group: {_id: '$fed', total: {$sum : 1}} }
     ]);
     query.exec((err, players) => {
         if (err) res.send(err);
         else {
             res.send(players.map(p => [ mapping[p._id], p.total ]));
+        }
+    });
+});
+
+app.get('/sort', function(req, res) {
+    // res.send('sort');
+    let query = Player.aggregate([
+         {$sort : {ukrRating: -1}},
+         {$project: {ukrRating: 1, _id: 0, lastName: 1}},
+         {$limit: 3000}
+    ])
+    query.exec((err, players) => {
+        if (err) res.send(err);
+        else res.send(players)
+    })
+});
+
+app.get('/age', function(req, res) {
+    let query = Player.aggregate([
+        { $match: {dob : {$gt: new Date('01 Jan 1970')}}},
+        { $project : {year : {$year : '$dob'}}},
+        { $group : {_id : '$year', count: {$sum : 1}}},
+        { $sort: {_id: -1}}
+    ]);
+    query.exec((err, players) => {
+        if (err) res.send(err);
+        else {
+            res.send(players);
+        }
+    });
+});
+
+app.get('/age-average-rating', function(req, res) {
+    let query = Player.aggregate([
+        { $match: {dob : {$gt: new Date('01 Jan 1970')}}},
+        { $project : {year : {$year : '$dob'}, ukrRating: 1}},
+        { $group : {_id : '$year', rating: {$avg : '$ukrRating'}}},
+        { $sort: {_id: -1}}
+    ]);
+    query.exec((err, players) => {
+        if (err) res.send(err);
+        else {
+            res.send(players);
+        }
+    });
+});
+
+app.get('/average-rating-by-fed', function(req, res) {
+    let query = Player.aggregate([
+        { $group : {_id : '$fed', rating: {$avg : '$ukrRating'}}},
+        { $sort: {rating: -1}}
+    ]);;
+    query.exec((err, players) => {
+        if (err) res.send(err);
+        else {
+            let minRating = Math.min(...players.map(p => p.rating))
+            console.log('min rating : ', minRating)
+            res.send(players.map(p => [ mapping[p._id], p.rating - minRating ]));
         }
     });
 });
@@ -68,7 +126,7 @@ function findAverage(fed) {
     });
     var sum = dob.reduce(function(a, b) { return a + b; });
     var avg = sum / dob.length;
-    return [mapping[fed._id], avg / 31536000000]
+    return [mapping[fed._id], +(avg / 31536000000).toFixed(2)]
 }
 
 const port = 3000;
--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
-function renderChart(data) {
-    Highcharts.mapChart('container', {
+function renderChart(data, containerID) {
+    Highcharts.mapChart(containerID, {
         chart: {
             map: 'countries/ua/ua-all'
         },
@@ -40,10 +40,79 @@ function renderChart(data) {
 
 }
 
-// fetch('/national-masters')
-//     .then(res => res.json())
-//     .then(renderChart)
 
-fetch('/average-age')
+function renderAge(cats, data, container) {
+        var options = {
+            chart: {
+                renderTo: container,
+                type: 'column'
+            },
+            title: {
+                text: 'Player count by age'
+            },
+            xAxis: {
+                categories: cats
+            },
+            yAxis: {
+                title: {
+                    text: 'number of players'
+                }
+            },
+            series: [{
+                data
+            }]
+        };
+
+    var chart = new Highcharts.Chart(options);
+}
+
+// fetch('/ratings/кмс')
+//     .then(res => res.json())
+//     .then((players) => renderChart(players, 'container'))
+
+// fetch('/ratings/1')
+//     .then(res => res.json())
+//     .then((players) => renderChart(players, 'container1'))
+
+// fetch('/age')
+//     .then(res => res.json())
+//     .then((players) => {
+//         const cats = players.map(p => p._id);
+//         const counts = players.map(p => p.count);
+//         renderAge(cats, counts, 'container25');
+//     })
+
+// fetch('/age-average-rating')
+//     .then(res => res.json())
+//     .then((players) => {
+//         const cats = players.map(p => p._id);
+//         const counts = players.map(p => p.rating);
+//         renderAge(cats, counts, 'container');
+// })
+
+function renderlist(data, container) {
+    // let html = '';
+}
+
+// fetch('/average-rating-by-fed')
+//     .then(res => res.json())
+//     .then((players) => {
+//         console.log(players)
+//         renderChart(players, 'container');
+// })
+
+fetch('/sort')
     .then(res => res.json())
-    .then(renderChart)
+    .then((players) => {
+        let html = '<table>';
+        players.forEach((p, i) => {
+            html += `<tr>
+                        <td>${i}</td>
+                        <td>${p.lastName}</td>
+                        <td>${p.ukrRating}</td>
+                    </tr>`
+        })
+        html += '</table>'
+        document.body.innerHTML = html;
+
+    })
