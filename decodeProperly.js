823cfbf94e293d87fe4e1f3ee9e5d51182635a7b
bodya17 <levantovychbogdan@gmail.com>
2017-07-13 23:12:36 +0300

    add rating schema

--- decodeProperly.js
+++ decodeProperly.js
@@ -2,7 +2,7 @@ const request = require('request');
 const fs = require('fs');
 const iconv = require('iconv-lite');
 
-request('http://ukrchess.org.ua/kvalif/1704/region/lvo.txt')
+request('http://ukrchess.org.ua/kvalif/1210/LUG.txt')
     .pipe(iconv.decodeStream('win1251'))
     .pipe(fs.createWriteStream('lvo.txt'))
 
--- models/Player.js
+++ models/Player.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose');
 
-const playerSchema = mongoose.Schema({
-    firstName: { type: String },
-    lastName: { type: String },
+const ratingSchema = mongoose.Schema({
     title: { type: String },
     gamesPlayed: { type: Number },
     ratingChange: { type: Number },
     ukrRating: { type: Number },
     fideRating: { type: Number },
+    expireDate: { type: Date },
+    date: { type: Date }
+});
+
+const playerSchema = mongoose.Schema({
+    firstName: { type: String },
+    lastName: { type: String },
     dob: { type: Date },
-    fed : { type: String },
     IDF: { type: String },
-    expireDate: { type: Date }
+    fed : { type: String },
+    ratings: [ratingSchema]
+        
 });
 
 const Player = mongoose.model('Players', playerSchema);
